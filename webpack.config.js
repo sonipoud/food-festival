bde50ca123552b3503db87bd00e6891212851764
Sonika <sonikapoudyal5@gmail.com>
2021-08-19 10:11:38 -0400

    images with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,27 @@ module.exports = {
         filename: "[name].bundle.js",
         path: __dirname + "/dist",
     },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
